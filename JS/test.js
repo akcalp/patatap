cfdc0895bb962cb5a5fa9731c5d54c6827ff54c4
akcalp <alp92akca@gmail.com>
2019-06-24 01:10:37 -0400

    before adding sound

--- JS/test.js
+++ JS/test.js
@@ -1,5 +1,6 @@
-for (var x = 0; x < 1000; x += 100) {
-    for (var y = 0; y < 1000; y += 100) {
-        new Path.Circle(new Point(x, y), 10).fillColor = "salmon";
-    }
+function onKeyDown(event) {
+
+    new Path.Circle(new Point(x, y), 10).fillColor = "salmon";
 }
+
+
